613525a5bef97f4bbad19b8e51c094d02c4abd8e
1393899065@qq.com <1393899065@qq.com>
2015-12-27 00:16:45 +0800

    #	README.md

--- app/src/main/java/com/zjnu/androidbike/activity/MainActivity.java
+++ app/src/main/java/com/zjnu/androidbike/activity/MainActivity.java
@@ -8,22 +8,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import com.zjnu.androidbike.R;
 
+import butterknife.Bind;
 import butterknife.BindString;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    //@Bind(R.id.user)
-    //EditText username;
+    @Bind(R.id.text_username)
+    EditText username;
 
-    //@Bind(R.id.pass)
-    //EditText password;
+    @Bind(R.id.text_password)
+    EditText password;
 
     @BindString(R.string.login_error)
     String loginErrorMessage;
+
+    @OnClick(R.id.button_login)
+    void button_login() {
+        System.out.println(username.getText());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
